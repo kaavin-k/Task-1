341b7d4f94a216012df484767bebb9350a4da29b
KAAVIN K <55978495+kaavin-k@users.noreply.github.com>
2022-11-07 17:34:24 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var data = 0;
-  document.getElementById("counting").innerText = data;
-  function increment() {
-      data = data + 1;
-      document.getElementById("counting").innerText = data;
-  }
